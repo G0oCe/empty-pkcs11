448c337c46ec1b80682d2e8d65f0f36ed6bfc5bb
G0oCe <alim.sadigov20@gmail.com>
2025-07-08 16:59:12 +0400

    create config

---
+++ src/pkcs11_config.h
@@ -0,0 +1,18 @@
+//
+// Created by goose on 7/8/25.
+//
+
+#ifndef EMPTY_PKCS11_PKCS11_CONFIG_H
+#define EMPTY_PKCS11_PKCS11_CONFIG_H
+
+// --- Версия стандарта PKCS#11, которому мы соответствуем ---
+#define CRYPTOKI_VERSION_MAJOR 2
+#define CRYPTOKI_VERSION_MINOR 40 // v2.40 - последняя на данный момент
+
+// --- Информация о нашей библиотеке ---
+#define LIBRARY_MANUFACTURER_ID "My Company Name"
+#define LIBRARY_DESCRIPTION     "PKCS#11 driver for SmartCard"
+#define LIBRARY_VERSION_MAJOR   1
+#define LIBRARY_VERSION_MINOR   0
+
+#endif //EMPTY_PKCS11_PKCS11_CONFIG_H
